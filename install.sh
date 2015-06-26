dd3ad92faa01fd24cd91d0759f6722d7d1964458
Devin Chen <d3vin.chen@gmail.com>
2015-06-26 16:26:20 +0800

    install to sling

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-export JAVA_HOME="/usr/java/jdk1.7.0_76" && ~/opt/apache-maven-3.3.3/bin/mvn install
+HOST="127.0.0.1"
+PORT="8888"
+
+#export JAVA_HOME="/usr/java/jdk1.7.0_76" && ~/opt/apache-maven-3.3.3/bin/mvn install
+export JAVA_HOME="/usr/java/jdk1.7.0_76" && ~/opt/apache-maven-3.3.3/bin/mvn -P autoInstallBundle clean install -Dsling.url=http://${HOST}:${PORT}/system/console
